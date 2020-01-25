c9d630318d804bb942c240f09f16677ac9c77619
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-01-25 03:05:05 -0800

    Adding initial server functionality only for dev backend

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 // This is the main server for drewwadsworth.com
 // make constant variable here root of the website
 // maybe want different constant variable root of dev.drewwadsworth.com
-// when we receive dev requests with url pathname = /client-dev-interface pass to client-dev-interface
 // otherwise, when we receive dev requests, just serve dev UI
 // redirect drewwadsworth.com/dev to dev.drewwadsworth.com
 // redirect http://drewwadsworth.com and http://dev.drewwadsworth.com to https with 308: permanent redirect
+
+const http = require('http');
+const url = require('url');
+
+const cdi = require('./client-dev-interface/client-dev-interface');
+
+const devRoot = '/Users/drewwadsworth/Documents/Developer/dev_root';
+const systemRoot = '/Users/drewwadsworth/Documents/Developer'
+const devURLRegex = /^\/client-dev-interface/;
+
+const server = http.createServer((request, response) => {
+    const query = url.parse(request.url, true);
+
+    // when we receive dev requests with url pathname = /client-dev-interface pass to client-dev-interface
+    if (devURLRegex.test(query.pathname)) {
+        cdi.handle(request, response, systemRoot);
+    }
+});
