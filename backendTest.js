123e911700be7b1670c0b2c28e9c72b02ba7600a
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-01-25 18:49:47 -0800

    Write main fileserver for entire site

--- backendTest.js
+++ backendTest.js
@@ -4,7 +4,7 @@ const dirLabel = "Directory: ";
 
 function formSubmit() {
     request.addEventListener("load", reqListener);
-    request.open("POST", "http://localhost:8080/create");
+    request.open("POST", "http://localhost:8080/client-dev-interface/create/");
     request.setRequestHeader('Content-Type', 'application/json');
     let body = {Directory : document.getElementById("inputField1").value,
                 Filename : document.getElementById("inputField2").value};
--- server.js
+++ server.js
@@ -7,18 +7,51 @@
 
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
 
 const cdi = require('./client-dev-interface/client-dev-interface');
 
 const devRoot = '/Users/drewwadsworth/Documents/Developer/dev_root';
-const systemRoot = '/Users/drewwadsworth/Documents/Developer'
+const siteRoot = '/Users/drewwadsworth/Documents/Developer/site_root';
+const systemRoot = '/Users/drewwadsworth/Documents/Developer';
 const devURLRegex = /^\/client-dev-interface/;
+const devSubDomRegex = /^dev./;
 
 const server = http.createServer((request, response) => {
     const query = url.parse(request.url, true);
 
+    // when we receive dev requests
+    const root = devSubDomRegex.test(request.headers['host']) ? devRoot : siteRoot;
+
     // when we receive dev requests with url pathname = /client-dev-interface pass to client-dev-interface
-    if (devURLRegex.test(query.pathname)) { //maybe use node url fileurltopath instead?
+    if (root === devRoot && devURLRegex.test(query.pathname)) { //maybe use node url fileurltopath instead?
         cdi.handle(request, response, systemRoot);
+    } else if (request.method === 'GET' || request.method === 'HEAD') {
+        // normal fileserver here
+        let file = root + query.pathname;
+        const dirPathRegex = /\/$/;     // file path ends in /
+        if (dirPathRegex.test(file)) {
+            file += 'index.html';
+        }
+        
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                response.writeHead(404, {'Content-Type': 'text/html'});
+                if (request.method != 'HEAD') response.write('Error 404: File Not Found');
+                response.end();
+            } else {
+                response.writeHead(200, {'Content-Type': mime.getType(path.extname(file))});
+                if (request.method != 'HEAD') response.write(data);
+                response.end();
+            }
+        });
+    } else {
+        response.writeHead(405, {'Content-Type': 'text/html', 'Allow': 'GET, HEAD'});
+        response.write('Error 405: Method Not Allowed');
+        response.end();
     }
 });
+
+server.listen(8080);
