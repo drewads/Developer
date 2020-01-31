e2158cde036de480507b01132b8eda80e11d8b5c
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-01-31 02:34:26 -0800

    Cleaning up some issues with create dev module, including filepath error in fs.access

--- dev_root/test/test.js
+++ dev_root/test/test.js
@@ -1,5 +1,5 @@
 // Create
-let createTest = (dir, filename, isDir) => {
+let createTest = (dir, filename, isDir, testName, callback) => {
     const request = new XMLHttpRequest();
     request.addEventListener("load", reqListener);
     request.open('PUT', 'http://dev.localhost:8080/client-dev-interface/create');
@@ -11,8 +11,30 @@ let createTest = (dir, filename, isDir) => {
 
     // shouldn't be arrow function
     function reqListener() {
-        alert(this.responseText);
+        const curElem = document.createElement("div");
+        curElem.innerText = testName + ': ' + this.responseText;
+        document.body.appendChild(curElem);
+        callback();
     }
 }
 
-createTest('/dev_root/test/hihi', 'hello', true);
+// requests need to be made in sequence because they are handled asynchronously on the server
+/*
+    0: work
+    1: work
+    2: fail
+    3: fail
+    4: work
+    5: fail
+*/
+createTest('/dev_root/test/hihi/', 'washang', true, 'Create Test 0', () => {
+    createTest('/dev_root/test/hihi/sup', 'hello', true, 'Create Test 1', () => {
+        createTest('/dev_root/test/hihi/sup/', '', false, 'Create Test 2', () => {
+            createTest('/dev_root/test/hihi/sup/', '', true, 'Create Test 3', () => {
+                createTest('/dev_root/test/hihi/sup/', 'hi', false, 'Create Test 4', () => {
+                    createTest('/dev_root/test/hihi/sup', 'hi', false, 'Create Test 5', () => {});
+                });
+            });
+        });
+    });
+});
