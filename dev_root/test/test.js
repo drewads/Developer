7a996936778f48841e36880534cf8118ea9c0bb8
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-02-02 02:38:25 -0800

    Adding promises to test file for dev modules

--- dev_root/test/test.js
+++ dev_root/test/test.js
@@ -1,25 +1,30 @@
-// Create
-const createTest = (dir, filename, isDir, testName, callback) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener("load", reqListenerCreate);
-    request.open('PUT', 'http://dev.localhost:8080/client-dev-interface/create');
-    request.setRequestHeader('Content-Type', 'application/json');
-    const body = {'Directory' : dir,
-                'Filename' : filename,
-                'isDirectory': isDir};
-    request.send(JSON.stringify(body));
+'use strict';
+const DONE_STATE = 4;
+// All requests need to be made in sequence because they are handled asynchronously on the server
 
-    // shouldn't be arrow function
-    function reqListenerCreate() {
-        const curElem = document.createElement("div");
-        curElem.innerText = testName + ': ' + this.responseText;
-        document.body.appendChild(curElem);
-        callback();
-    }
+// Create
+const createTest = (dir, filename, isDir, testName) => {
+    return new Promise(resolve => {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState === DONE_STATE) {
+                const curElem = document.createElement("div");
+                curElem.innerText = testName + ': ' + request.response;
+                document.body.appendChild(curElem);
+                resolve();
+            }
+        }
+        request.open('PUT', 'http://dev.localhost:8080/client-dev-interface/create');
+        request.setRequestHeader('Content-Type', 'application/json');
+        const body = {'Directory' : dir,
+                    'Filename' : filename,
+                    'isDirectory': isDir};
+        request.send(JSON.stringify(body));
+    });
 }
 
-// requests need to be made in sequence because they are handled asynchronously on the server
 /*
+    Create
     0: work
     1: work
     2: fail
@@ -27,34 +32,34 @@ const createTest = (dir, filename, isDir, testName, callback) => {
     4: work
     5: fail
 */
-// use promises here instead
-createTest('/dev_root/test/hihi/', 'washang', true, 'Create Test 0', () => {
-    createTest('/dev_root/test/hihi/sup', 'hello', true, 'Create Test 1', () => {
-        createTest('/dev_root/test/hihi/sup/', '', false, 'Create Test 2', () => {
-            createTest('/dev_root/test/hihi/sup/', '', true, 'Create Test 3', () => {
-                createTest('/dev_root/test/hihi/sup/', 'hi', false, 'Create Test 4', () => {
-                    createTest('/dev_root/test/hihi/sup', 'hi', false, 'Create Test 5', () => {});
-                });
-            });
-        });
-    });
-});
+
+createTest('/dev_root/test/hihi/', 'washang', true, 'Create Test 0')
+.then(() => createTest('/dev_root/test/hihi/sup', 'hello', true, 'Create Test 1'))
+.then(() => createTest('/dev_root/test/hihi/sup/', '', false, 'Create Test 2'))
+.then(() => createTest('/dev_root/test/hihi/sup/', '', true, 'Create Test 3'))
+.then(() => createTest('/dev_root/test/hihi/sup/', 'hi', false, 'Create Test 4'))
+.then(() => createTest('/dev_root/test/hihi/sup', 'hi', false, 'Create Test 5'))
+.catch(error => alert('Something went wrong with Create tests.'))
+.then(() => document.body.appendChild(document.createElement('br')));
+
 
 
 // Delete
-// add promises to this to do the same thing as the callbacks above
 const deleteTest = (filepath, isDir, testName) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener('load', reqListenerDelete);
-    request.open('DELETE', 'http://dev.localhost:8080/client-dev-interface/delete');
-    request.setRequestHeader('Content-Type', 'application/json');
-    const body = {'Filepath': filepath,
-                'isDirectory': isDir};
-    request.send(JSON.stringify(body));
-
-    function reqListenerDelete() {
-        const curElem = document.createElement("div");
-        curElem.innerText = testName + ': ' + this.responseText;
-        document.body.appendChild(curElem);
-    }
+    return new Promise(resolve => {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState === DONE_STATE) {
+                const curElem = document.createElement("div");
+                curElem.innerText = testName + ': ' + request.response;
+                document.body.appendChild(curElem);
+                resolve();
+            }
+        }
+        request.open('DELETE', 'http://dev.localhost:8080/client-dev-interface/delete');
+        request.setRequestHeader('Content-Type', 'application/json');
+        const body = {'Filepath': filepath,
+                    'isDirectory': isDir};
+        request.send(JSON.stringify(body));
+    });
 }
