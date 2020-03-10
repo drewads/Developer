da3fa03eafc58e7d7da09ba0594bcf18b242fccf
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-03-09 21:34:52 -0700

    Finishing upload dev module tests and tweaks post-testing

--- dev_root/test/uploadTest.js
+++ dev_root/test/uploadTest.js
@@ -11,8 +11,13 @@ const DONE_STATE = 4; // when the HTTP request is completely finished, including
   * files in the body of the single request if multiple files are
   * selected. In the multipart/form-data body, the key for each file
   * is the filepath it should be saved at on the server.
+  * 
+  * @param files files from an input element
+  * @param {string} dir the directory on the server to save the files to
+  * @param {string} requestMethod the HTTP request method
+  * @return {Promise} a Promise that is resolved when HTTP response is received
   */
-const uploadFiles = (files, dir, requestMethod) => {
+const uploadFiles = (files, dir, requestMethod, altBody = null) => {
     return new Promise(resolve => {
         const uploadForm = new FormData();
 
@@ -30,31 +35,52 @@ const uploadFiles = (files, dir, requestMethod) => {
         }
 
         upload.open(requestMethod, 'http://dev.localhost:8080/client-dev-interface/upload');
-        upload.send(uploadForm);
+        upload.send((altBody ? altBody : uploadForm));
     });
+}
+
+/**
+ * This function adds test results to the document.
+ * 
+ * @param {string} response HTTP response body
+ * @param {string} expected expected HTTP response body
+ * @param {number} testNumber number of this test
+ */
+const printResults = (response, expected, testNumber) => {
+    const elem = document.createElement("div");
+    
+    if (expected) {
+        elem.innerText = 'Upload Test ' + testNumber + '. '
+                    + (response === expected ? 'success' : 'failure')
+                    + ': ' + response;
+        elem.style.color = (response === expected ? 'green' : 'red');
+    } else {
+        elem.innerText = 'Upload Test ' + testNumber + ': ' + response
+                        + '\nWARNING: make sure to delete files that ended up in temporary directory!';
+    }
+    
+    document.body.appendChild(elem);
 }
 
 // trigger uploadFiles() when files are selected with the filepicker input element
 document.getElementById('upload').addEventListener('change', () => {
     const files = document.getElementById('upload').files;
-    const dir = '/dev_root/test/';
-    uploadFiles(files, dir, 'PUT')
-    .then((response) => {
-        const elem = document.createElement("div");
-        elem.innerText = 'Upload Test 1. '
-                        + (response === 'file(s) successfully uploaded' ? 'success' : 'failure')
-                        + ': ' + response;
-        document.body.appendChild(elem);
-    }).catch((error) => alert('problem with Upload Test 1.\n' + error));
+    uploadFiles(files, '/dev_root/test/', 'PUT')
+    .then((response) => {printResults(response, 'file(s) successfully uploaded', 0);})
+    // add test that has directory that is above filesystem to get isDescendantOf error
+    // the following tests save files to temporary directory and then doesn't move them - watch out
+    .then(() => uploadFiles(files, '/dev_root/../../', 'PUT'))
+    .then((response) => printResults(response, null, -1))
+    // could use test to make rename not work - maybe incorrect dir
+    .then(() => uploadFiles(files, '/dev_root/this/doesnt/exist', 'PUT'))
+    .then((response) => printResults(response, null, -2))
+    .catch((error) => alert('problem with negatively numbered Upload Tests.\n' + error));
 });
 
-// add test with incorrect method - will now get incorrect method
-
-// add test that has directory that is above filesystem to get isDescendantOf error
-
-// add test to make rename not work - maybe incorrect dir
-
+// test with incorrect method
+uploadFiles([], '/dev_root/test/', 'DELETE')
+.then((response) => printResults(response, 'method not allowed', 1))
 // add test with incorrect multipart/form-data body - will now get 'request body has incorrect format'
-
-// add test with some other body type - will now get 'request body has incorrect format'
-
+.then(() => uploadFiles([], '/dev_root/test/', 'PUT', 'hello'))
+.then((response) => printResults(response, 'request body has incorrect format', 3))
+.catch((error) => alert('problem with positively numbered Upload Tests.\n' + error));
