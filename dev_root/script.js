6f7356fb14784100e1ea866aa0b9c8e38e0bbbcb
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-03-14 15:51:12 -0700

    Finishing style and functionality prior to writing UI in React

--- dev_root/script.js
+++ dev_root/script.js
@@ -1,51 +1,4 @@
-/*************************** Move/Rename Elements Toggling **********************************/
-let move_rename_engaged = false;
-
-for (const button of document.getElementsByClassName('FSButton')) {
-    button.addEventListener('click', event => {
-        event.preventDefault();
-        if (move_rename_engaged) {
-            for (const elem of document.getElementsByClassName('moveElems')) {
-                elem.style.visibility = 'hidden';
-            }
-            document.getElementById('moveFrom').style.display = 'none';
-            document.getElementById('generalInput').style.width = '100%';
-            move_rename_engaged = false;
-        } else if (button.id === 'move_rename') {
-            for (const elem of document.getElementsByClassName('moveElems')) {
-                elem.style.visibility = 'visible';
-            }
-            document.getElementById('moveFrom').style.display = 'block';
-            document.getElementById('generalInput').style.width = '90%';
-            move_rename_engaged = true;
-        }
-    });
-}
-
-/*************************** Create Editor and Add Value to it **********************************/
-const editor = CodeMirror.fromTextArea(document.getElementById('editorTextArea'), {
-    lineNumbers: true
-});
-
-editor.setSize('100%', '100%');
-
-
-const DONE_STATE = 4; // when the HTTP request is completely finished, including response
-
-const request = new XMLHttpRequest();
-
-// when we get the entire HTTP response back from the server
-request.onreadystatechange = () => {
-    if (request.readyState === DONE_STATE) {
-        editor.setValue(request.response);
-        editor.setOption('mode', request.getResponseHeader('Content-Type'));
-    }
-}
-
-// devModule has no file extension
-request.open('GET', 'http://dev.localhost:8080/client-dev-interface/edit?Filepath=/dev_root/script.js');
-
-request.send();
+const DONE_STATE = 4; // when HTTP request is completely finished, including response
 
 /*************************** Select SystemObjects function **********************************/
 
@@ -53,10 +6,8 @@ let selectedObject = null; // maybe wanna use react to create something where on
                     // there is a component that handles everything and stores all the data
                     // if i was gonna do this without react, i would make this a class
 
-const systemObjects = document.getElementsByClassName("systemObject");
-
-for (const o of systemObjects) {
-    o.addEventListener("click", () => {selectObject(o)});
+const setSystemObjectEvents = (object) => {
+    object.addEventListener("click", () => {selectObject(object)});
 }
 
 document.getElementById("navWrapper").addEventListener("click", () => {unselectObject()}, true);
@@ -90,3 +41,172 @@ function unselectObject() {
     document.getElementById("manageFSForm").value = "";
     selectedObject = null;
 }
+
+/*************************** Move/Rename Elements Toggling **********************************/
+const dirRequest = new XMLHttpRequest();
+// when we get the entire HTTP response back from the server
+dirRequest.onreadystatechange = () => {
+    if (dirRequest.readyState === DONE_STATE) {
+        if (dirRequest.status === 200) {
+            const elements = JSON.parse(dirRequest.response);
+            const navViewer = document.getElementById('navViewer');
+            for (const element of elements) {
+                const outerDiv = document.createElement('div');
+                outerDiv.className = 'systemObject';
+                const img = document.createElement('img');
+                img.src = (element['isDir'] ? 'GenericFolderIcon.png' : 'GenericDocumentIcon.png')
+                const label = document.createElement('div');
+                label.className = 'systemObjectLabel';
+                label.innerText = element['name'];
+                outerDiv.append(img);
+                outerDiv.append(label);
+                navViewer.append(outerDiv);
+                setSystemObjectEvents(outerDiv);
+            }
+        } else {
+            alert(dirRequest.response);
+        }
+    }
+}
+
+// devModule has no file extension
+dirRequest.open('GET', 'http://dev.localhost:8080/client-dev-interface/dir-snapshot?Directory=/');
+
+dirRequest.send();
+
+/*************************** Move/Rename Elements Toggling **********************************/
+
+let move_rename_engaged = false;
+let create_dropdown_engaged = false;
+
+const engageMoveRename = () => {
+    for (const elem of document.getElementsByClassName('moveElems')) {
+        elem.style.visibility = 'visible';
+    }
+    document.getElementById('moveFrom').style.display = 'block';
+    document.getElementById('generalInput').style.width = '90%';
+    move_rename_engaged = true;
+}
+
+const engageCreateDropdown = () => {
+    document.getElementById('createDropdown').style.display = 'block';
+    create_dropdown_engaged = true;
+}
+
+const disengageMoveRename = () => {
+    for (const elem of document.getElementsByClassName('moveElems')) {
+        elem.style.visibility = 'hidden';
+    }
+    document.getElementById('moveFrom').style.display = 'none';
+    document.getElementById('generalInput').style.width = '100%';
+    move_rename_engaged = false;
+}
+
+const disengageCreateDropdown = () => {
+    document.getElementById('createDropdown').style.display = 'none';
+    create_dropdown_engaged = false;
+}
+
+const createClicked = (event) => {
+    event.preventDefault();
+    
+    if (move_rename_engaged) {
+        disengageMoveRename();
+    }
+    
+    if (create_dropdown_engaged) {
+        disengageCreateDropdown();
+    } else {
+        engageCreateDropdown();
+    }
+}
+
+const createFileClicked = () => {
+    disengageCreateDropdown();
+}
+
+const createDirClicked = () => {
+    disengageCreateDropdown();
+}
+
+const editClicked = (event) => {
+    event.preventDefault();
+    
+    if (move_rename_engaged) {
+        disengageMoveRename();
+    }
+    
+    if (create_dropdown_engaged) {
+        disengageCreateDropdown();
+    }
+}
+
+const uploadClicked = (event) => {
+    event.preventDefault();
+    
+    if (move_rename_engaged) {
+        disengageMoveRename();
+    }
+    
+    if (create_dropdown_engaged) {
+        disengageCreateDropdown();
+    }
+
+    document.getElementById('uploadInput').click();
+}
+
+const moveRenameClicked = (event) => {
+    event.preventDefault();
+    
+    if (create_dropdown_engaged) {
+        disengageCreateDropdown();
+    }
+
+    if (move_rename_engaged) {
+        disengageMoveRename();
+    } else {
+        engageMoveRename();
+    }
+}
+
+const deleteClicked = (event) => {
+    event.preventDefault();
+
+    if (move_rename_engaged) {
+        disengageMoveRename();
+    }
+    
+    if (create_dropdown_engaged) {
+        disengageCreateDropdown();
+    }
+}
+
+document.getElementById('create').addEventListener('click', event => createClicked(event));
+document.getElementById('edit').addEventListener('click', event => editClicked(event));
+document.getElementById('upload').addEventListener('click', event => uploadClicked(event));
+document.getElementById('move_rename').addEventListener('click', event => moveRenameClicked(event));
+document.getElementById('delete').addEventListener('click', event => deleteClicked(event));
+document.getElementById('createFile').addEventListener('click', event => createFileClicked(event));
+document.getElementById('createDir').addEventListener('click', event => createDirClicked(event));
+
+/*************************** Create Editor and Add Value to it **********************************/
+const editor = CodeMirror.fromTextArea(document.getElementById('editorTextArea'), {
+    lineNumbers: true
+});
+
+editor.setSize('100%', '100%');
+
+const request = new XMLHttpRequest();
+
+// when we get the entire HTTP response back from the server
+request.onreadystatechange = () => {
+    if (request.readyState === DONE_STATE) {
+        editor.setValue(request.response);
+        editor.setOption('mode', request.getResponseHeader('Content-Type'));
+    }
+}
+
+// devModule has no file extension
+request.open('GET', 'http://dev.localhost:8080/client-dev-interface/edit?Filepath=/dev_root/script.js');
+
+request.send();
